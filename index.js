b606bc830b32e0ed9178826bec2263aa8dd65329
liad madmon <madmonl@post.bgu.ac.il>
2020-06-24 16:00:44 +0300

    Add client

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
 
 const app = express();
 
+app.use(express.static(path.join(__dirname, 'client', 'build')));
+app.use(bodyParser.json());
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+});
+
 const port = process.env.PORT || 5000;
 app.listen(port);
-
-console.log(`form-builder is listening on port ${port}`);

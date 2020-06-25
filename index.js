f2996ea45a264f8aee186ca2b8fe0b7954cad164
liad madmon <madmonl@post.bgu.ac.il>
2020-06-25 16:37:13 +0300

    Add db configuration

--- index.js
+++ index.js
@@ -1,23 +1,20 @@
 const express = require('express');
-const path = require('path');
 const bodyParser = require('body-parser');
+const db = require('./firebase/firebase');
 
 const app = express();
-
-const videoDetails = {
-  likes: 0,
-  views: 20
-};
-
-app.use(express.static(path.join(__dirname, 'client', 'build')));
 app.use(bodyParser.json());
 
-app.post('video', async (req, res) => {
-  res.send(videoDetails);
+app.get('/api/video', async (req, res) => {
+  let videoDetails = (await db.ref('video').once('value')).val();
+  await db.ref('video/likes').set(++videoDetails.views);
+  res.json(videoDetails);
 });
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+app.get('/api/likes/increase', async (req, res) => {
+  let { likes } = (await db.ref('video').once('value')).val();
+  await db.ref('video/likes').set(++likes);
+  res.json(likes);
 });
 
 const port = process.env.PORT || 5000;

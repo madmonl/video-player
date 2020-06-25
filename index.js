1e15e8b08c4869cebb90059dda44bf6454af17fd
liad madmon <madmonl@post.bgu.ac.il>
2020-06-25 16:38:25 +0300

    Add heroku support

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const path = require('path');
 const bodyParser = require('body-parser');
 const db = require('./firebase/firebase');
 
 const app = express();
+
+app.use(express.static(path.join(__dirname, 'client', 'build')));
 app.use(bodyParser.json());
 
 app.get('/api/video', async (req, res) => {
@@ -15,6 +18,10 @@ app.get('/api/likes/increase', async (req, res) => {
   let { likes } = (await db.ref('video').once('value')).val();
   await db.ref('video/likes').set(++likes);
   res.json(likes);
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
 });
 
 const port = process.env.PORT || 5000;
